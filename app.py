135d984a0dc0411cdd1502ceb11b1ce32932a46f
Rajendra Ayer <rajendra.ayer@outlook.com>
2025-06-19 19:57:55 +1000

    Final commit

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
 import streamlit as st
+from skops.io import load
+import pandas as pd
 
 st.title("House Price Prediction App")
+
+# Load the trained model
+model=load("model/model.skops")
+
+
+# User input 
+area=st.number_input("Put Area of land")
+bedrooms=st.number_input("Number of beds")
+age=st.number_input("Age of house")
+
+user_data=pd.DataFrame({
+    'Area_sqft':[area],
+    'Bedrooms':[bedrooms],
+    'Age_years':[age]
+})
+
+# prediction of house price
+if st.button("Predict"):
+    predict=model.predict(user_data)
+    st.write(f"The predicted house price is{predict}")
